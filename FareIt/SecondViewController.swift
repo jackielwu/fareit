57d61e44f6890772045ea31369a9d750762ad3bb
Richard Ju <Richard@pal-nat186-140-113.itap.purdue.edu>
2015-10-17 11:43:01 -0400

    Distance Preference Buttons
    Added a stepper to modify the distance prefer values, also added 3
    buttons (walk, bike, drive) that automatically input fixed values
    commonly used for those modes of transportation.

--- FareIt/SecondViewController.swift
+++ FareIt/SecondViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBAction func bikeSelected(sender: UIButton) {
+        distancePref.text = "5"
+    }
+    @IBAction func driveSelected(sender: UIButton) {
+        distancePref.text = "15"
+    }
+    @IBAction func walkSelected(sender: UIButton) {
+        distancePref.text = "1"
+    }
+    @IBOutlet weak var BikeDistance: UIButton!
+    @IBOutlet weak var DriveDistance: UIButton!
+    @IBOutlet weak var walkDistance: UIButton!
+    @IBAction func distPrefValChanged(sender: UIStepper) {
+        
+        distancePref.text = Int(sender.value).description
+    }
+    @IBOutlet weak var distancePref: UITextField!
+    @IBOutlet weak var distancePrefAdjuster: UIStepper!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        distancePrefAdjuster.wraps = true
+        distancePrefAdjuster.autorepeat = true
+        distancePrefAdjuster.maximumValue = 30
+        distancePrefAdjuster.minimumValue = 0.5
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
