d53853e66488777ffcad97740b5dd696b57576c1
Jackie Wu <jackielwu@yahoo.com>
2015-10-17 19:01:03 -0400

    bug fix

--- FareIt/Business.swift
+++ FareIt/Business.swift
@@ -91,6 +91,6 @@ class Business: NSObject {
     
     
     class func searchWithTerm(term: String, sort: YelpSortMode?, categories:[String]?, deals: Bool?, completion: ([Business]!, NSError!) -> Void) -> Void {
-        YelpClient.sharedInstance.searchWithTerm(term, sort: sort, categories, deals: deals, completion: completion)
+        YelpClient.sharedInstance.searchWithTerm(term, sort: sort, categories: categories, deals: deals, completion: completion)
     }
 }
