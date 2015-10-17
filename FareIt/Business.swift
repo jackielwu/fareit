e4ca45941f28e9f7119ac132ffd2fdd9923cd1c3
Jackie Wu <jackielwu@yahoo.com>
2015-10-17 13:08:22 -0400

    added draft of business.swift

---
+++ FareIt/Business.swift
@@ -0,0 +1,63 @@
+//
+//  Business.swift
+//  FareIt
+//
+//  Created by Jackie Wu on 10/17/15.
+//  Copyright © 2015 FareIt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Business: NSObject {
+    let name: String?
+    let address: String?
+    let imageURL: NSURL?
+    let categories: String?
+    let distance: String?
+    let ratingImageURL: NSURL?
+    let ratingCount: NSNumber?
+    
+    init(dictionary: NSDictionary) {
+        name = dictionary["name"] as? String
+        
+        let imageURLString = dictionary["image_url"] as? String
+        if imageURLString != nil {
+            imageURL = NSURL(string: imageURLString)
+        } else {
+            imageURL = nil
+        }
+        
+        let location = dictionary["location"] as? NSDictionary
+        var address = ""
+        if location != nil {
+            let addressArray = location!["address"] as? NSArray
+            var street: String? = ""
+            if addressArray != nil && addressArray!.count > 0 {
+                address = addressArray![0] as! String
+            }
+            
+            let neighborhoods = location!["neighborhoods"] as? NSArray
+            if neighborhoods != nil && neighborhoods!.count > 0 {
+                if !address.isEmpty {
+                    address += ", "
+                }
+                address += neighborhoods![0] as! String
+            }
+        }
+        self.address = address
+        
+        let categoriesArray = dictionary["categories"] as? [[String]]
+        if categoriesArray != nil {
+            var categoryNames = [String]()
+            for category in categoriesArray! {
+                let categoryName = category[0]
+                categoryNames.append(categoryName)
+            }
+        } else {
+            categories = nil
+        }
+        
+        
+    }
+}
