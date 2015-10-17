1bad72ed5d5e8b9dd28b752df55742bc203727fe
potatoshane <apple@pal-nat186-52-181.itap.purdue.edu>
2015-10-17 15:35:29 -0400

    Added Facebook SDK
    Changed AppDelegate for Facebook Login and Facebook Integration

--- FareIt/FirstViewController.swift
+++ FareIt/FirstViewController.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, FBSDKLoginButtonDelegate {
     @IBOutlet weak var facebookConnect: UIButton!
     @IBOutlet weak var googleConnect: UIButton!
     @IBOutlet weak var indivList: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        facebookConnect.layer.cornerRadius = 10
+        
+        if (FBSDKAccessToken.currentAccessToken() == nil) {
+            print("Not logged in.")
+        } else {
+        print("Logged in.")
+        }
+        
+        var loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        
+        self.view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +37,19 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Facebook Login
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error == nil {
+            print("Login complete.")
+        } else {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User logged out...")
+    }
 
 }
 
