136748728466e6912782d2979d56a6cb324cf6d6
Jackie Wu <jackielwu@yahoo.com>
2015-10-17 14:36:58 -0400

    added Yelp-Bridging-Header.h

---
+++ FareIt/Yelp-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Yelp-Bridging-Header.h
+//  FareIt
+//
+//  Created by Jackie Wu on 10/17/15.
+//  Copyright © 2015 FareIt. All rights reserved.
+//
+
+#ifndef Yelp_Bridging_Header_h
+#define Yelp_Bridging_Header_h
+
+#import "NSDictionary+BDBOAuth1Manager.h"
+#import "BDBOAuth1RequestOperationManager.h"
+#import "UIImageView+AFNetworking.h"
+
+#endif /* Yelp_Bridging_Header_h */
